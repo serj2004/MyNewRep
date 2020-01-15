000fa4ce42f4a3fb9eff818f0ba8c841daab4904
Serj2004 <serg_2004@inbox.ru>
2020-01-15 23:08:08 +0700

    new version

--- conftest.py
+++ conftest.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-options = Options()
-user_language = None
-options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-browser = webdriver.Chrome(options=options)
+# options = Options()
+# language = None
+# options.add_experimental_option('prefs', {'intl.accept_languages': language})
+# browser = webdriver.Chrome(options=options)
 
 
 def pytest_addoption(parser):
@@ -14,23 +14,24 @@ def pytest_addoption(parser):
                      help="Choose language: es or fr")
 
 
-@pytest.fixture(scope="function")
-def language(request, user_language):
-    user_language = request.config.getoption("language")
-    if user_language == "es":
-        print("\nstart chrome browser for test..")
-    elif user_language == "fr":
-        print("\nstart firefox browser for test..")
-    else:
-        raise pytest.UsageError("--language should be es or fr")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+# @pytest.fixture(scope="function")
+# def language(request, language):
+#     user_language = request.config.getoption("language")
+#     if user_language == "es":
+#         print("\nstart chrome browser for test..")
+#     elif user_language == "fr":
+#         print("\nstart firefox browser for test..")
+#     else:
+#         raise pytest.UsageError("--language should be es or fr")
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
 
 
 @pytest.fixture(scope="function")
-def browser():
-    # browser = webdriver.Chrome()
-    # browser.implicitly_wait(60)
+def browser(request):
+    language = request.config.getoption("language")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(60)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @pytest.mark.parametrize('language', ["es", "fr"])
 def test_items(browser, language):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element_by_xpath("(//button[contains(@class,'btn btn-lg')])[1]")
     assert button.is_displayed()
